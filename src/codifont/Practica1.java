7515905a0c364a5f7d8e91861901be8d70da374c
AlbaRiius <75982732+AlbaRiius@users.noreply.github.com>
2021-02-16 11:15:01 +0100

    Update Practica1.java
    Afegeixo un comentari a la linia 17

--- src/codifont/Practica1.java
+++ src/codifont/Practica1.java
@@ -14,6 +14,7 @@ import java.util.Arrays;
 public class Practica1 {
 
     public static void main(String[] args) {
+        //El canvi és aquest comentari
 
         //Prova1: retornarà null i v1 contindrà [ 3, 4, 5, 6]
         int[] v1 = new int[]{1, 2, 3, 4};
